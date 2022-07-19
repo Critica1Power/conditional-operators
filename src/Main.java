b75fa783d796c9a2296c7944dcbb4ecfead8a722
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-07-19 20:53:44 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -44,5 +44,14 @@ public class Main {
 
         // Задание 3
 
+        int year = 2024;
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year +" год является високосным");
+        } else {
+            System.out.println(year +" год не является високосным");
+        }
+
+
     }
 }
