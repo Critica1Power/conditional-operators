5ddf6c198e7de663effdfa9016e4402ab666928a
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-07-19 20:27:38 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {task1(); task2(); task3();}
+
+    public static void task1() {
+
         // Задание 1
 
-    int clientOS = 0;
-    if (clientOS == 0) {
-        System.out.println("Установите версию приложения для iOS по ссылке");
-    } else {
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+    }
+
+    public static void task2() {
 
         // Задание 2
+
+        int ClientOS = 0;
+        int clientDeviceYear = 2016;
+
+        if (ClientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
+          else {
+              if (clientDeviceYear < 2015) {
+                  System.out.println("Установите облегченную версию приложения для Android по ссылке");
+              }
+              else {
+                  System.out.println("Установите версию приложения для Android по ссылке");
+
+            }
+        }
+
+
+    }
+
+    public static void task3() {
+
+        // Задание 3
 
     }
 }
