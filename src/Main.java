894876f25e58682cd68d03532d35607a2d4092eb
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-07-19 19:42:58 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        // Задание 1
 
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+    int clientOS = 0;
+    if (clientOS == 0) {
+        System.out.println("Установите версию приложения для iOS по ссылке");
+    } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        // Задание 2
+
     }
 }
